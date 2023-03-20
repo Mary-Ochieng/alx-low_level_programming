d6bb8a95c235953719694b8b35e49737f68f5f88
mary <glormary47@gmail.com>
2023-03-20 12:49:47 -0700

    new updates variables, else if and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,26 @@
 # include <stdio.h>
 
 /**
-  *main - prints if number is positive,zero or negative
+  *main - Entry point
+  *Description; 'prints if number is positive,zero or negative'
   *return; always 0 success
-  *
   */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand( ) - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive", n);
-
-else if ( n==0 )
-printf("%d is zero", n) ;
-
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 else
-printf("%d is negative", n);
-printf("\n")
+{
+printf("%d is negative\n", n);
+}
 return (0);
 }
