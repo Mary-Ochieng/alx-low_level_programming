93f28173d805f1dda693050de8ef104241985eb8
mary <glormary47@gmail.com>
2023-03-20 14:49:28 -0700

    print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,17 @@ int main(void)
 	{
 		for (j = 1; j < 10; j++)
 		{
-			if (i < j && i != j);
-			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i + j != 17);
+				for (j = 1; j < 10; j++)
 				{
+					if (i + j != 17);
+					{
+
 					putchar(',');
 					putchar(' ');
+					}
 				}
-			}
 		}
 	}
 	putchar('\n');
