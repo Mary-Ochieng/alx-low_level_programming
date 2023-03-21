76789d6fa0b64ba2e15e321b03931f4b54992aca
mary <glormary47@gmail.com>
2023-03-21 11:02:08 -0700

    new change

--- 0x02-functions_nested_loops/.vimrc
+++
@@ -1,3 +0,0 @@
-set nonautoindent
-set nonnumber
-set nonautoindent
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-# if nder main_h
-# define main_h
+#if nder main_
+#define main_h
 
 int _putchar(char c);
 void print_alphabet(void);
