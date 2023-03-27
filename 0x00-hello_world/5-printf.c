44690adae14054008dc83e436a1ffc81d7649f89
mary <glormary47@gmail.com>
2023-03-27 06:01:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *return : always (0) success
+  * main - entry point
+  * description:to print 'with proper grammer, but the outcome is a piece o'
+  * return : always (0) success
   */
+
+/* this prototype has to be declared in a header file */
 int main(void)
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 	}
